65d8be53f82b541a5fdc57deff7531faa007f58c
MizukiAmia <hyunjun3028@gmail.com>
2024-06-14 13:58:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,38 @@
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
-void func(int cnt) {
-   
-
-
-
-
-
-
+void func(vector<int> seq) {
+	int last;
+	if (seq.empty()) {
+		last = 0;
+	}
+	else {
+		last = seq.back(); //last
+	}
+	if (last <= N - M + seq.size()) {
+		if (seq.size() == M) { //print
+			for (int i = 0; i < M; i++) {
+				cout << seq[i];
+			}
+			cout << endl;
+		}
+		else {
+			int k = 1;
+			while (last+k <= N) {
+				vector<int> seq2 = seq;
+				seq2.push_back(last + k);
+				func(seq2);
+				k++;
+			}
+		}
+	}
 }
 
+
 int main() {
-   cin >> N >> M;
-   func(0);
+	cin >> N >> M;
+	func(vector<int> {});
 }
